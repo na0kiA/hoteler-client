53c38a3b5a1d1212f76572c9e33e8621115b01bf
na0kiA <na0ki199823@gmail.com>
2022-12-20 22:38:05 +0900

    [fix]スネークケースになっていたtypeをキャメルケースに修正1

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,39 +1,39 @@
 export type HotelFacilityType = {
-  wifi_enabled: boolean;
-  parking_enabled: boolean;
-  credit_card_enabled: boolean;
-  phone_reservation_enabled: boolean;
-  net_reservation_enabled: boolean;
-  triple_rooms_enabled: boolean;
-  secret_payment_enabled: boolean;
-  cooking_enabled: boolean;
-  breakfast_enabled: boolean;
-  coupon_enabled: boolean;
+  wifiEnabled: boolean;
+  parkingEnabled: boolean;
+  creditCardEnabled: boolean;
+  phoneReservationEnabled: boolean;
+  netReservationEnabled: boolean;
+  tripleRoomsEnabled: boolean;
+  secretPaymentEnabled: boolean;
+  cookingEnabled: boolean;
+  breakfastEnabled: boolean;
+  couponEnabled: boolean;
 };
 
 export type ReviewType = {
   title: string;
   content: string;
-  five_star_rate: number;
-  helpfulnesses_count: number;
-  user_name: string;
-  user_image: string;
-  created_at: Date;
+  fiveStarRate: number;
+  helpfulnessesCount: number;
+  userName: string;
+  userImage: string;
+  createdAt: Date;
 };
 
 export type HotelDetailType = {
   name: string;
-  favorites_count: number;
+  favoritesCount: number;
   content: string;
   company: string;
   phone_number: string;
   postal_code: string;
-  full_address: string;
-  hotel_facilities: HotelFacilityType;
+  fullAddress: string;
+  hotelFacilities: HotelFacilityType;
   full: boolean;
   average_rating: number;
-  reviews_count: number;
-  hotel_images: string;
-  day_of_the_week: string;
-  top_four_reviews: ReviewType;
+  reviewsCount: number;
+  hotelImages: string;
+  dayOfTheWeek: string;
+  topFourReviews: ReviewType;
 };
