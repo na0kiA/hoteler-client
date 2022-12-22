ba771cbaab27da9e5491b014bef569eb3f0f599e
na0kiA <na0ki199823@gmail.com>
2022-12-22 15:43:11 +0900

    [dev]トップページに表示するためのヘッダーを追加#1

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Link from "next/link";
+// import Link from "next/link";
 // import Image from 'next/image'
 import React from "react";
+import Header from "./Header";
 
 type TITLE = {
   title: string;
@@ -14,22 +15,7 @@ const Layout: React.FC<TITLE> = ({ children, title = "ホテラー" }) => {
       <Head>
         <title>{title}</title>
       </Head>
-      <header>
-        <nav className="bg-gray-800 w-screen">
-          <div className="flex items-center pl-8 h-14">
-            <div className="flex space-x-4">
-              <Link href="/">
-                <a
-                  data-testid="blog-nav"
-                  className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
-                >
-                  Blog
-                </a>
-              </Link>
-            </div>
-          </div>
-        </nav>
-      </header>
+      <Header />
       <main className="flex flex-1 justify-center items-center flex-col w-screen">
         {children}
       </main>
