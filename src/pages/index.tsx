ed0171745deaa4c0b3749f8151fac6cd5e4cf052
na0kiA <na0ki199823@gmail.com>
2022-12-24 02:30:29 +0900

    [dev]CSSはgridにして、カード型のホテルの要素を横に４つ並べるように開発#1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetServerSideProps } from "next";
 
 import { getAllHotel } from "lib/allRequests";
 import { HotelListType, ServiceRateType } from "types/types";
@@ -10,7 +10,7 @@ type PROPS = {
   hotels: HotelListType[];
 };
 
-const Home: NextPage<PROPS> = ({ hotels }) => {
+const Home = ({ hotels }: PROPS) => {
   const opneOrClsed = (service: ServiceRateType[]) => {
     if (typeof service === "string") {
       return service;
@@ -23,25 +23,50 @@ const Home: NextPage<PROPS> = ({ hotels }) => {
     }
   };
 
+  const sliceNameOrNot = (name: string) => {
+    if (name.length > 6) {
+      return name.slice(0, 5).concat("...");
+    } else {
+      return name;
+    }
+  };
+
   return (
     <>
       <Layout title={"ホテラー"}>
-        <div>
-          <h2>ホテル一覧</h2>
-          <h2>
-            {hotels &&
-              hotels.map((hotel: HotelListType) => (
-                <div key={hotel.id}>
-                  <Link href={`/hotels/${hotel.id}`}>{hotel.name}</Link>
-                  <p>{hotel.hotelImages}</p>
-                  <p>{hotel.full}</p>
-                  <p>{hotel.averageRating}</p>
-                  <p>{hotel.reviewsCount}</p>
+        <div className="h-screen grid grid-cols-4 gap-5 p-10">
+          {hotels &&
+            hotels.map((hotel: HotelListType) => (
+              <div
+                key={hotel.id}
+                className="card-bordered card-compact w-68 bg-base-100 shadow-xl"
+              >
+                <figure>
+                  <img
+                    src="https://placeimg.com/400/225/arch"
+                    alt="ホテル画像"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-base">
+                    <Link href={`/hotels/${hotel.id}`}>
+                      {sliceNameOrNot(hotel.name)}
+                    </Link>
+                    <div className="badge badge-secondary">
+                      {hotel.full ? "満室" : "空室"}
+                    </div>
+                  </h2>
+                  <p className="card-title text-xs leading-none text-opacity-100">
+                    {hotel.fullAddress}
+                  </p>
+                  <p className="leading-none">
+                    ({hotel.averageRating}) {hotel.reviewsCount}件
+                  </p>
                   {opneOrClsed(hotel.restRates)}
                   {opneOrClsed(hotel.stayRates)}
                 </div>
-              ))}
-          </h2>
+              </div>
+            ))}
         </div>
       </Layout>
     </>
