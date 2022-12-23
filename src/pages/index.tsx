460f0778e698d5b4624641170cb4c847fb3109c2
na0kiA <na0ki199823@gmail.com>
2022-12-23 16:14:00 +0900

    [dev]サービスが営業しているかどうかを条件分岐で表示するように開発#1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import type { GetServerSideProps, NextPage } from "next";
 
 import { getAllHotel } from "lib/allRequests";
-import { HotelListType } from "types/types";
+import { HotelListType, ServiceRateType } from "types/types";
 import Layout from "components/Layout";
 
 type PROPS = {
@@ -11,31 +11,38 @@ type PROPS = {
 };
 
 const Home: NextPage<PROPS> = ({ hotels }) => {
+  const opneOrClsed = (service: ServiceRateType[]) => {
+    if (typeof service === "string") {
+      return service;
+    } else {
+      return (
+        <p>
+          {service[0].plan} {service[0].rate}円
+        </p>
+      );
+    }
+  };
+
   return (
     <>
       <Layout title={"ホテラー"}>
-        <div className="bg-black">
+        <div>
           <h2>ホテル一覧</h2>
           <h2>
             {hotels &&
               hotels.map((hotel: HotelListType) => (
-                <li key={hotel.id}>
+                <div key={hotel.id}>
                   <Link href={`/hotels/${hotel.id}`}>{hotel.name}</Link>
                   <p>{hotel.hotelImages}</p>
                   <p>{hotel.full}</p>
                   <p>{hotel.averageRating}</p>
                   <p>{hotel.reviewsCount}</p>
-                  <p>
-                    {hotel.restRates[0].plan} {hotel.restRates[0].rate}円
-                  </p>
-                  <p>
-                    {hotel.stayRates[0].plan} {hotel.stayRates[0].rate}円
-                  </p>
-                </li>
+                  {opneOrClsed(hotel.restRates)}
+                  {opneOrClsed(hotel.stayRates)}
+                </div>
               ))}
           </h2>
         </div>
-        <div className="bg-black"></div>
       </Layout>
     </>
   );
