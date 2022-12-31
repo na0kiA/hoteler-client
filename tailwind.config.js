239b37349c37dc3111fcf33a1ff2c3adb9986b8b
na0kiA <na0ki199823@gmail.com>
2022-12-31 23:17:19 +0900

    [dev]darkmodeを追加#1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
-  // daisyui: {
-  //   themes: ["black"],
-  // },
+  daisyui: {
+    themes: ["dark"],
+  },
   theme: {
     extend: {
       colors: {
         'blue-link': '#6FE9FE',
       },
+      fontSize: {
+        ssm: '0.6rem',
+      }
     },
   },
   plugins: [require("daisyui")],
