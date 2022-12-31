f2a4b750734debf4710f9613c393f1b953a15625
na0kiA <na0ki199823@gmail.com>
2022-12-31 15:15:09 +0900

    [add]jestのプラグインを追加#1

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true
   },
   extends: ["plugin:react/recommended", "standard", "prettier"],
   parser: "@typescript-eslint/parser",
@@ -12,6 +13,6 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint","jest"],
   rules: { "react/prop-types": "off" },
 };
