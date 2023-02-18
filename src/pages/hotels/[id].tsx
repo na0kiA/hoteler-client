cec9bcbcc6491179a3553ebd232fc30692a9523c
na0kiA <na0ki199823@gmail.com>
2023-02-19 02:25:33 +0900

    [clean]eslintに沿って不必要なモジュールや定数の削除#5

--- src/pages/hotels/[id].tsx
+++ src/pages/hotels/[id].tsx
@@ -290,15 +290,32 @@ const HotelDetail = ({
               "電話予約",
               "/電話予約.svg"
             )}
+            {/* {facilityBadge(
+              hotelFacilities.creditCardEnabled,
+              "クレジットカード",
+              "/クレジットカードのフリーアイコン (1).svg"
+            )} */}
+            <div className="flex text-base w-full my-1 md:w-1/2 md:p-3 md:m-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
+                />
+              </svg>
+              <div className="ml-3">クレジットカード</div>
+            </div>
             {facilityBadge(
               hotelFacilities.netReservationEnabled,
               "ネット予約",
               "/ネット予約.svg"
-            )}
-            {facilityBadge(
-              hotelFacilities.creditCardEnabled,
-              "クレジットカード決済可能",
-              "/クレジットカード.svg"
             )}
             {facilityBadge(
               hotelFacilities.breakfastEnabled,
