3748f50a4332e024c83d05d10f1bb9e42d45583b
na0kiA <na0ki199823@gmail.com>
2023-03-22 03:42:31 +0900

    [fix]og imageを修正#9

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,8 +17,8 @@ const Layout = memo(function layout({ children, title }: TITLE) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           property="og:image"
-          // content="https://jp.lovehoteler.com/hartIcon.png"
-          content="https://hoteler-image-list.s3.ap-northeast-1.amazonaws.com/hartIcon.png"
+          content="https://jp.lovehoteler.com/hartIcon.png"
+          // content="https://hoteler-image-list.s3.ap-northeast-1.amazonaws.com/hartIcon.png"
         />
       </Head>
       <main>
